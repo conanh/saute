dea9f67bebee3db1b51c32175dd6f3fa51593acb
BuildTools <unconfigured@null.spigotmc.org>
2020-10-19 09:44:30 -0400

    Added general page layout markup & top nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
 import RecipeList from './components/RecipeList';
 import Recipe from './components/Recipe';
 import AddRecipe from './components/AddRecipe';
 import './App.css';
 
+const { Header, Content, Footer } = Layout;
+
 function App() {
+  const [currentMenu, setCurrentMenu] = useState('');
+
+  const handleClick = e => {
+    console.log('click ', e);
+    setCurrentMenu(e.key);
+  };
+
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/recipes" component={RecipeList} />
-        <Route path="/recipes/add" component={AddRecipe} />
-        <Route path="/recipes/:id" component={Recipe} />
-      </Switch>
+
+      <Layout className="layout">
+        <Header>
+        <Link to="/"><div className="logo" /></Link>
+          <Menu theme="dark" mode="horizontal" selectedKeys={[currentMenu]} onClick={handleClick}>
+            <Menu.Item key="recipes">
+              <Link to="/recipes">Recipes</Link>
+            </Menu.Item>
+            <Menu.Item key="planning">
+              <Link>Meal Planning</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div className="site-layout-content">
+            <Switch>
+              <Route exact path="/recipes" component={RecipeList} />
+              <Route path="/recipes/add" component={AddRecipe} />
+              <Route path="/recipes/:id" component={Recipe} />
+            </Switch>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Saute ©2020</Footer>
+      </Layout>
+
+
     </BrowserRouter>
   );
 }
